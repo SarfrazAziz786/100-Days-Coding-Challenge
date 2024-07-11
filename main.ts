e0de92fe361b08d30755dc89b01d641607fd68aa
Sarfraz Abdul Aziz <sarfraz.aziz786@hotmail.com>
2024-07-11 22:05:53 +0500

    4th Day of 100 Days Codding Challenge

--- main.ts
+++ main.ts
@@ -65,5 +65,30 @@ console.log(5 + 3);
 let favorite_number: number = 7;
 console.log(`My favorite number is "${favorite_number}"`);
 
+// DAY 4
+
+//10)Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+//If you don’t have anything specific to write because your programs are too simple at this point, just 
+//add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+person_name = "sarfraz";
+//this program print a personal msg
+console.log(`Hello, ${person_name} would you like to learn some Python today?`);
+
+//11) Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element
+// in the list, one at a time.
+
+let friends_list: string[] = ["sajjad", "waseem", "danish", "sumair"];
+
+for (let i = 0; i <= 3; i++) {
+  console.log(friends_list[i]);
+}
+
+// 12) Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them.
+// The text of each message should be the same, but each message should be personalized with the person’s name.
+
+for (let name of friends_list) {
+  console.log(`Hello ${name} would you like to learn some typesript today?`);
+}
 
 
